7bf758a0385489db2f767c7ae89d48737a27bb5b
Michael Johnson <mjohnson@ociaw.com>
2019-09-12 02:20:11 -0500

    Implement Merseene Twister

--- cuhogaus/MersenneTwister.cs
+++ cuhogaus/MersenneTwister.cs
@@ -1,17 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Buffers.Binary;
 
 namespace Cuhogaus
 {
+    /// <summary>
+    /// An implementation of MT19937-64.
+    /// Based on the pseudocode provided from https://en.wikipedia.org/wiki/Mersenne_twister.
+    /// </summary>
     public sealed class MersenneTwister : IRng
     {
-        private const int WordSize = sizeof(uint) * 8;
-        private const int RecurrenceDegree = 624;
-        private const int MiddleWord = 397;
-        private const int OneWordSeparationPoint = 31;
+        private const int WordSize = sizeof(ulong) * 8; // 64 bits (w)
+        private const int RecurrenceDegree = 312; // (n)
+        private const int MiddleWord = 156; // (m)
+        private const int OneWordSeparationPoint = 31; // (r)
 
-        private const uint TwistMatrixCoefficients = 0x9908B0DF;
+        private const ulong LowerMask = (1ul << OneWordSeparationPoint) - 1;
+        private const ulong UpperMask = ~LowerMask;
 
+        private const ulong TwistMatrixCoefficients = 0xB5026F5AA96619E9; // (a)
+
+        // TGSFSR(R) tempering bitmasks
+        private const ulong BMask = 0x71D67FFFEDA60000;
+        private const ulong CMask = 0xFFF7EEE000000000;
+
+        // TGSFSR(R) tempering bit shifts
+        private const int SShift = 17;
+        private const int TShift = 37;
+
+        // Additional tempering bit shifts and masks
+        private const int LShift = 43;
+        private const int UShift = 29;
+        private const ulong DMask = 0x5555555555555555;
+
+        private const ulong F = 6364136223846793005;
+
+        private ulong[] _state;
+        private Int32 _index;
+
+        private MersenneTwister(ulong[] state)
+        {
+            _state = state;
+            _index = state.Length;
+        }
+
+        public uint NextUInt32() => UInt64Filler.ToUInt32(NextUInt64());
+
+        public ulong NextUInt64()
+        {
+            if (_index == _state.Length)
+                Twist();
+
+            ulong y = _state[_index];
+            y ^= (y >> UShift) & DMask;
+            y ^= (y << SShift) & BMask;
+            y ^= (y << TShift) & CMask;
+            y ^= y >> LShift;
+
+            _index++;
+
+            return y;
+        }
+
+        public void Fill(byte[] buffer) => UInt64Filler.Fill(buffer, NextUInt64);
+
+        public void Fill(Span<Byte> buffer) => UInt64Filler.Fill(buffer, NextUInt64);
+
+        private void Twist()
+        {
+            for (int i = 0; i < _state.Length; i++)
+            {
+                ulong upper = _state[i] & UpperMask;
+                ulong lower = _state[(i + 1) % _state.Length] & LowerMask;
+                ulong x = upper + lower;
+
+                ulong xA = x >> 1;
+                if (x % 2 == 0)
+                    xA ^= TwistMatrixCoefficients;
+
+                _state[i] = _state[(i + MiddleWord) % RecurrenceDegree] ^ xA;
+            }
+
+            _index = 0;
+        }
+
+        public sealed class Factory : IRngFactory
+        {
+            public Int32 SeedLength => sizeof(ulong);
+
+            public IRng Create(ReadOnlySpan<Byte> seed)
+            {
+                // Init state from seed
+                ulong[] state = new ulong[RecurrenceDegree];
+                BinaryPrimitives.TryReadUInt64LittleEndian(seed, out state[0]);
+
+                for (uint i = 1; i < state.Length; i++)
+                {
+                    state[i] = F * (state[i - 1] ^ (state[i - 1] >> (WordSize - 2))) + i;
+                }
+
+                return new MersenneTwister(state);
+            }
+        }
     }
 }
---
+++ cuhogaus/UInt64Filler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Buffers.Binary;
+
+namespace Cuhogaus
+{
+    internal sealed class UInt64Filler
+    {
+        public static uint ToUInt32(ulong num) => (uint)(num);
+
+        public static void Fill(byte[] buffer, Func<ulong> source) => Fill(buffer.AsSpan(), source);
+
+        public static void Fill(Span<byte> buffer, Func<ulong> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var current = buffer;
+            while (current.Length != 0)
+            {
+                ulong num = source();
+                Int32 written = FillInternal(current, num);
+
+                if (current.Length == written)
+                    return; // The buffer is now filled.
+
+                current = current.Slice(current.Length);
+            }
+        }
+
+        private static Int32 FillInternal(Span<byte> buffer, ulong num)
+        {
+            Span<byte> bytes = stackalloc byte[sizeof(uint)];
+            BinaryPrimitives.TryWriteUInt64LittleEndian(bytes, num);
+
+            Int32 length = Math.Min(buffer.Length, bytes.Length);
+            for (int i = 0; i < length; i++)
+                buffer[i] = bytes[i];
+
+            return length;
+        }
+    }
+}
