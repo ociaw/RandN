780da5c06d11a8ca13225dd5c5d9ebfe2dbcfae3
Michael Johnson <mjohnson@ociaw.com>
2020-04-22 12:30:38 -0500

    Add ICryptoRng marker interface

---
+++ cuhogaus/ICryptoRng.cs
@@ -0,0 +1,9 @@
+﻿namespace Cuhogaus
+{
+    /// <summary>
+    /// A marker interface used to indicate that an <see cref="IRng"/>
+    /// is cryptographically secure.
+    /// </summary>
+    public interface ICryptoRng : IRng
+    { }
+}
