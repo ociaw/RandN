0bf820fbe30794202299bf7f52fb57f975d05e9d
Michael Johnson <mjohnson@ociaw.com>
2020-05-07 15:11:10 -0500

    Add WIP pcg32 implementation

---
+++ Rand/Pcg32.cs
@@ -0,0 +1,114 @@
+﻿// Copyright 2018 Developers of the Rand project.
+// Copyright 2017 Paul Dicker.
+// Copyright 2014-2017 Melissa O'Neill and PCG Project contributors
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+using Rand.RngHelpers;
+using System;
+
+namespace Rand
+{
+    /// <summary>
+    /// A PCG random number generator (XSH RR 64/32 (LCG) variant).
+    /// </summary>
+    /// <remarks>
+    /// Permuted Congruential Generator with 64-bit state, internal Linear
+    /// Congruential Generator, and 32-bit output via "xorshift high (bits),
+    /// random rotation" output function.
+    ///
+    /// This is a 64-bit LCG with explicitly chosen stream with the PCG-XSH-RR
+    /// output function. This combination is the standard pcg32.
+    ///
+    /// Despite the name, this implementation uses 16 bytes (128 bit) space
+    /// comprising 64 bits of state and 64 bits stream selector. These are both set
+    /// by <see cref="Pcg32.Factory"/>, using a 128-bit seed.
+    /// </remarks>
+    public sealed class Pcg32 : IRng
+    {
+        private const UInt64 MULTIPLIER = 6364136223846793005;
+
+        private UInt64 _state;
+        private readonly UInt64 _increment;
+
+        private Pcg32(UInt64 state, UInt64 increment)
+        {
+            _increment = increment;
+            _state = unchecked(state + _increment);
+            Step();
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="Pcg32"/> instance, treating <paramref name="state"/> and <paramref name="stream"/>
+        /// the same way as the reference implementation of PCG.
+        /// </summary>
+        /// <param name="state">The 64 bit state.</param>
+        /// <param name="stream">The 63 bit stream id - the highest bit is ignored.</param>
+        public static Pcg32 Create(UInt64 state, UInt64 stream)
+        {
+            // The increment must be odd.
+            var increment = (stream << 1) | 1;
+            return new Pcg32(state, increment);
+        }
+
+        public UInt32 NextUInt32()
+        {
+            var state = _state;
+            Step();
+
+            // Output function XSH RR: xorshift high (bits), followed by a random rotate
+            // Constants are for 64-bit state, 32-bit output
+            const Int32 ROTATE = 59; // 64 - 5
+            const Int32 XSHIFT = 18; // (5 + 32) / 2
+            const Int32 SPARE = 27; // 64 - 32 - 5
+
+            var rotation = (Int32)(state >> ROTATE);
+            var xShift = (UInt32)(((state >> XSHIFT) ^ state) >> SPARE);
+            // Rotate Right
+            return (xShift >> rotation) | (xShift << (32 - rotation));
+        }
+
+        public UInt64 NextUInt64() => Filler.NextUInt64ViaUInt32(this);
+
+        public void Fill(Span<Byte> buffer) => Filler.FillBytesViaNext(this, buffer);
+
+        private void Step() => _state = unchecked(_state * MULTIPLIER + _increment);
+
+        public sealed class Factory : IReproducibleRngFactory<Pcg32, Seed>
+        {
+            public Pcg32 Create(Seed seed) => new Pcg32(seed.State, seed.Stream | 1);
+
+            public Seed CreateSeed<TSeedingRng>(TSeedingRng seedingRng) where TSeedingRng : IRng
+            {
+                return new Seed(seedingRng.NextUInt64(), seedingRng.NextUInt64());
+            }
+        }
+
+        /// <summary>
+        /// A 127-bit seed to initialize the state and select a stream.
+        /// </summary>
+        public readonly struct Seed
+        {
+            public Seed(UInt64 state, UInt64 stream)
+            {
+                State = state;
+                Stream = stream;
+            }
+
+            /// <summary>
+            /// The 64-bit seed state.
+            /// </summary>
+            public UInt64 State { get; }
+
+            /// <summary>
+            /// The 63 bit stream id. Unlike the reference implementation of PCG, the lowest bit is ignored
+            /// instead of the highest bit.
+            /// </summary>
+            public UInt64 Stream { get; }
+        }
+    }
+}
