aa1f010aacb06d1f113354387f287fdb0a06b8ed
Michael Johnson <mjohnson@ociaw.com>
2020-05-31 23:57:26 -0500

    Add ThreadLocalRng

---
+++ RandN/ThreadLocalRng.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using RandN.Rngs;
+
+namespace RandN
+{
+    /// <summary>
+    /// A cryptographically secure thread local generator. All members in this type are thread-safe.
+    /// </summary>
+    public sealed class ThreadLocalRng : IRng, ICryptoRng
+    {
+        private static readonly ThreadLocal<ChaCha> _threadLocal = new ThreadLocal<ChaCha>(() =>
+        {
+            using var seeder = CryptoServiceProvider.Create();
+            return ChaCha.GetChaCha8Factory().Create(seeder);
+        });
+
+        /// <summary>
+        /// Creates a <see cref="ThreadLocalRng"/>.
+        /// </summary>
+        public static ThreadLocalRng Get() => new ThreadLocalRng();
+
+        /// <inheritdoc />
+        public void Fill(Span<Byte> buffer) => _threadLocal.Value.Fill(buffer);
+
+        /// <inheritdoc />
+        public UInt32 NextUInt32() => _threadLocal.Value.NextUInt32();
+
+        /// <inheritdoc />
+        public UInt64 NextUInt64() => _threadLocal.Value.NextUInt64();
+    }
+}
