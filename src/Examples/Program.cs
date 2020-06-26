81d031c2f34a541b0f37086feac6a9f6609a0a98
Michael Johnson <mjohnson@ociaw.com>
2020-06-25 19:31:33 -0500

    Add examples project

---
+++ src/Examples/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Diagnostics;
+using RandN.Distributions;
+using RandN.Rngs;
+
+namespace RandN.Examples
+{
+    public sealed class Program
+    {
+        static void Main()
+        {
+            var seed = new ChaCha.Seed(new UInt32[8] { 1, 2, 3, 4, 5, 6, 7, 8 });
+            var rng = ChaCha.Create(seed);
+
+            var stopwatch = Stopwatch.StartNew();
+            var (pi, error) = EstimatePi(rng);
+            stopwatch.Stop();
+            Console.WriteLine($"Pi: {pi}, error: {error}, time (sec): {stopwatch.Elapsed.TotalSeconds}");
+        }
+
+        /// <summary>
+        /// Estimates Pi using a Monte Carlo simulation.
+        /// </summary>
+        public static (Double pi, Double error) EstimatePi<TRng>(TRng rng) where TRng : IRng
+        {
+            const UInt64 iterations = 400_000_000;
+            var dist = UnitInterval.ClosedDouble.Instance;
+            UInt64 insideQuadrant = 0;
+            for (var i = 0ul; i < iterations; i++)
+            {
+                var x = dist.Sample(rng);
+                var y = dist.Sample(rng);
+                var mag2 = x * x + y * y;
+                if (mag2 <= 1.0)
+                    insideQuadrant += 1;
+            }
+
+            Double error = 1 / Math.Sqrt(iterations);
+            return ((Double)insideQuadrant / iterations * 4, error);
+        }
+    }
+}
