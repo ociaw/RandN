bc73f11349cddca9ed808aed91842a69f9462d3a
Michael Johnson <mjohnson@ociaw.com>
2020-11-27 20:02:25 -0600

    Add tests covering the Mersenne Twister RNG

--- src/Tests/Rngs/MtTests.cs
+++ src/Tests/Rngs/MtTests.cs
@@ -1028,6 +1028,32 @@ namespace RandN.Rngs
         }
 
         [Fact]
+        public void FactoryArrayCreation()
+        {
+            var seedRng = new StepRng(0xF0F0_F0F0_F0F0_F0F0);
+            var factory = Mt1993764.GetFactory();
+            var seed = factory.CreateSeed(seedRng);
+            var rng = factory.Create(seed);
+
+            Assert.True(Statistics.TestMonobitFrequency32(rng, 100_000, Statistics.WideZScore));
+            Assert.True(Statistics.TestMonobitFrequency64(rng, 100_000, Statistics.WideZScore));
+            Assert.True(Statistics.TestMonobitFrequencyFill(rng, 100_000, Statistics.WideZScore));
+        }
+
+        [Fact]
+        public void FactoryIntCreation()
+        {
+            var seedRng = new StepRng(0xF0F0_F0F0_F0F0_F0F0);
+            IReproducibleRngFactory<Mt1993764, UInt64> factory = Mt1993764.GetFactory();
+            var seed = factory.CreateSeed(seedRng);
+            var rng = factory.Create(seed);
+
+            Assert.True(Statistics.TestMonobitFrequency32(rng, 100_000, Statistics.WideZScore));
+            Assert.True(Statistics.TestMonobitFrequency64(rng, 100_000, Statistics.WideZScore));
+            Assert.True(Statistics.TestMonobitFrequencyFill(rng, 100_000, Statistics.WideZScore));
+        }
+
+        [Fact]
         public void NonNullable()
         {
             Assert.Throws<ArgumentNullException>(() => Mt1993764.GetFactory().CreateSeed<StepRng>(null));
