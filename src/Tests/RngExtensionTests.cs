4b08e0d220ef7a64c4dccb4f7c975d8ee295e4fe
Michael Johnson <mjohnson@ociaw.com>
2020-11-28 01:56:20 -0600

    Add test covering the remainder of RngExtensions

--- src/Tests/RngExtensionTests.cs
+++ src/Tests/RngExtensionTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using RandN.Distributions;
 using Xunit;
 
 namespace RandN
@@ -15,6 +16,16 @@ namespace RandN
             Assert.Equal(new Int32[] { 2, 3, 4, 5, 6, 7, 1 }, list);
 
             Assert.Throws<ArgumentNullException>(() => rng.ShuffleInPlace((List<Int32>)null));
+        }
+
+        [Fact]
+        public void Sample()
+        {
+            var dist = Bernoulli.FromRatio(1, 2);
+            var rng = new StepRng(0);
+
+            var result = rng.Sample<StepRng, Bernoulli, Boolean>(dist);
+            Assert.True(result);
         }
     }
 }
