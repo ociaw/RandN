f35c7b2ecf6e981dca6a48a8309649a3756413bb
Michael Johnson <mjohnson@ociaw.com>
2022-01-25 20:50:46 -0900

    Add basic tests for SystemCryptoRng

---
+++ src/Tests/Rngs/SystemCryptoRngTests.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace RandN.Rngs
+{
+    /// <summary>
+    /// Very basic testing of <see cref="SystemCryptoRng"/>.
+    /// </summary>
+    public sealed class SystemCryptoRngTests
+    {
+        [Fact]
+        public void Construction()
+        {
+            var factory = SystemCryptoRng.GetFactory();
+            using var rng = factory.Create();
+            Assert.True(Statistics.TestMonobitFrequency32(rng, 100_000, Statistics.WideZScore));
+            Assert.True(Statistics.TestMonobitFrequency64(rng, 100_000, Statistics.WideZScore));
+            Assert.True(Statistics.TestMonobitFrequencyFill(rng, 100_000, Statistics.WideZScore));
+        }
+    }
+}
