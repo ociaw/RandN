06c32a6a0673036763fa5928f5d3f18a853dab02
Michael Johnson <mjohnson@ociaw.com>
2022-02-02 17:10:06 -0900

    Support use of the MaybeNullWhenAttribute in Rand project
    Since the MaybeNullWhenAttribute in Core is internal, we need an internal copy in Rand as well.

---
+++ src/RandN/MaybeNullWhenAttribute.cs
@@ -0,0 +1,22 @@
+// ReSharper disable All
+#if NETSTANDARD2_0
+namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>
+    /// This is a hacky way to allow us to use the MaybeNullWhenAttribute when we target .NET Standard 2.0. 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    internal sealed class MaybeNullWhenAttribute : Attribute
+    {
+        /// <summary>
+        /// Creates a new instance of <see cref="MaybeNullWhenAttribute"/>.
+        /// </summary>
+        public MaybeNullWhenAttribute(Boolean returnValue) => ReturnValue = returnValue;
+
+        /// <summary>
+        /// The return value when the parameter may be null.
+        /// </summary>
+        public Boolean ReturnValue { get; }
+    }
+}
+#endif
