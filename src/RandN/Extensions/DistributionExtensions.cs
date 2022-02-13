ed1c012b7244fad05b0f1a9793a777bd3603578d
Dan Carter <djcarter85@gmail.com>
2022-02-13 20:01:46 +0000

    Implement overload of `SelectMany()` with a result selector

--- src/RandN/Extensions/DistributionExtensions.cs
+++ src/RandN/Extensions/DistributionExtensions.cs
@@ -41,7 +41,33 @@ namespace RandN.Extensions
         public static IDistribution<TResult> SelectMany<TSource, TResult>(
             this IDistribution<TSource> distribution,
             Func<TSource, IDistribution<TResult>> selector) =>
-            new SelectManyDistribution<TSource, TResult>(distribution, selector);
+            distribution.SelectMany(selector, (_, x) => x);
+
+        /// <summary>
+        /// Transforms a distribution by mapping values using the selector provided to produce
+        /// a new distribution, which is then sampled from.
+        /// </summary>
+        /// <example>
+        /// This method enables use of LINQ query syntax over <see cref="IDistribution{TResult}"/>. For example:
+        /// <code>
+        /// IDistribution&lt;PointF&gt; unitSquarePointDistribution =
+        ///     from x in Uniform.NewInclusive(0.0, 1.0)
+        ///     from y in Uniform.NewInclusive(0.0, 1.0)
+        ///     select new System.Drawing.PointF(x, y);
+        /// </code>
+        /// </example>
+        /// <typeparam name="TSource">The generic type of the input distribution.</typeparam>
+        /// <typeparam name="TIntermediate">The generic type of the intermediate distribution.</typeparam>
+        /// <typeparam name="TResult">The generic type of the output distribution.</typeparam>
+        /// <param name="distribution">The distribution to be transformed.</param>
+        /// <param name="selector">The projection to be applied to values from the
+        /// distribution to produce an intermediate distribution.</param>
+        /// <param name="resultSelector">The projection to be applied to values from both distributions.</param>
+        public static IDistribution<TResult> SelectMany<TSource, TIntermediate, TResult>(
+            this IDistribution<TSource> distribution,
+            Func<TSource, IDistribution<TIntermediate>> selector,
+            Func<TSource, TIntermediate, TResult> resultSelector) =>
+            new SelectManyDistribution<TSource, TIntermediate, TResult>(distribution, selector, resultSelector);
 
         private sealed class SelectDistribution<TSource, TResult> : IDistribution<TResult>
         {
@@ -75,22 +101,28 @@ namespace RandN.Extensions
             }
         }
 
-        private sealed class SelectManyDistribution<TSource, TResult> : IDistribution<TResult>
+        private sealed class SelectManyDistribution<TSource, TIntermediate, TResult> : IDistribution<TResult>
         {
             private readonly IDistribution<TSource> _distribution;
-            private readonly Func<TSource, IDistribution<TResult>> _selector;
+            private readonly Func<TSource, IDistribution<TIntermediate>> _selector;
+            private readonly Func<TSource, TIntermediate, TResult> _resultSelector;
 
-            public SelectManyDistribution(IDistribution<TSource> distribution, Func<TSource, IDistribution<TResult>> selector)
+            public SelectManyDistribution(
+                IDistribution<TSource> distribution,
+                Func<TSource, IDistribution<TIntermediate>> selector,
+                Func<TSource, TIntermediate, TResult> resultSelector)
             {
                 _distribution = distribution;
                 _selector = selector;
+                _resultSelector = resultSelector;
             }
 
             public TResult Sample<TRng>(TRng rng) where TRng : notnull, IRng
             {
-                var sample = _distribution.Sample(rng);
-                var distribution = _selector(sample);
-                return distribution.Sample(rng);
+                var sample1 = _distribution.Sample(rng);
+                var distribution = _selector(sample1);
+                var sample2 = distribution.Sample(rng);
+                return _resultSelector(sample1, sample2);
             }
 
             public Boolean TrySample<TRng>(TRng rng, [MaybeNullWhen(false)] out TResult result) where TRng : notnull, IRng
@@ -109,7 +141,7 @@ namespace RandN.Extensions
                     return false;
                 }
 
-                result = sample2;
+                result = _resultSelector(sample1, sample2);
                 return true;
             }
         }
