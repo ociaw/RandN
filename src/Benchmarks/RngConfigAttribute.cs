643a3b725977cb6ec47254621eb795940372f3ce
Michael Johnson <mjohnson@ociaw.com>
2024-06-09 18:29:53 -0500

    Fix BenchmarkDotNet refusing to run on non-Windows platforms

--- src/Benchmarks/RngConfigAttribute.cs
+++ src/Benchmarks/RngConfigAttribute.cs
@@ -12,13 +12,14 @@ internal sealed class RngConfigAttribute : Attribute, IConfigSource
         BytesPerIteration = bytesPerIteration;
         var standard = new Job("All Enabled");
         var noAvx2 = new Job("AVX2 Disabled").WithEnvironmentVariable("COMPlus_EnableAVX2", "0");
-        var netFramework = Job.Default.WithRuntime(BenchmarkDotNet.Environments.ClrRuntime.Net48).WithId(".NET 4.8");
 
         Config = ManualConfig.Create(DefaultConfig.Instance)
             .AddColumn(new ThroughputColumn(BytesPerIteration))
             .AddJob(standard)
-            .AddJob(noAvx2)
-            .AddJob(netFramework);
+            .AddJob(noAvx2);
+
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            Config = Config.AddJob(Job.Default.WithRuntime(BenchmarkDotNet.Environments.ClrRuntime.Net48).WithId(".NET 4.8"));
     }
 
     public UInt64 BytesPerIteration { get; }
